516682d392a9bc0dc728f4a542bdf2b2e7c6b8f2
dontmatterbro <yh846175@gmail.com>
2024-09-13 09:41:01 +0300

    file organization
    removed unnecessary files
    reformatted init.lua
    changed mod name

--- NT Hyperbaric Medicine/Lua/Autorun/init.lua
+++ NT Hyperbaric Medicine/Lua/Autorun/init.lua
@@ -1,28 +1,30 @@
 
-NTHM = {} -- Neurotrauma Symbiote
-NTHM.Name="Symbiote"
-NTHM.Version = "A1.0.0h0"
+NTHM = {} -- Neurotrauma Hyperbaric
+NTHM.Name="Hyperbaric"
+NTHM.Version = "A1.0.0"
 NTHM.VersionNum = 01000000
 NTHM.MinNTVersion = "A1.9.0"
 NTHM.MinNTVersionNum = 01090000
 NTHM.Path = table.pack(...)[1]
 Timer.Wait(function() if NTC ~= nil and NTC.RegisterExpansion ~= nil then NTC.RegisterExpansion(NTHM) end end,1)
 
--- server-side code (also run in singleplayer)
-if (Game.IsMultiplayer and SERVER) or not Game.IsMultiplayer then
+Timer.Wait(function()
 
-    Timer.Wait(function()
-        if NTC == nil then
-            print("Error loading NT Hyperbaric Medicine: It appears Neurotrauma isn't loaded!")
-            return
-        end
-
-        -- dofile(NTHM.Path.."/Lua/Scripts/helperfunctions.lua")
-
-        -- dofile(NTHM.Path.."/Lua/Scripts/humanupdate.lua")
-        -- dofile(NTHM.Path.."/Lua/Scripts/items.lua")
-        -- dofile(NTHM.Path.."/Lua/Scripts/testing.lua")
-
-    end,1)
-
-end
+	if NTC == nil then
+		print("Error loading NT Hyperbaric: It appears Neurotrauma isn't loaded!")
+		return
+	end
+	
+		--server side or singleplayer scripts
+	if SERVER or (CLIENT and not Game.IsMultiplayer) then
+		--dofile(NTHM.Path.."/Lua/Server/template.lua")
+	end
+	
+	
+		--client side only scripts (in case we want to add visuals)
+	if CLIENT then
+		--dofile(NTHM.Path.."/Lua/Client/template.lua")
+	end
+	
+	
+end,1)
