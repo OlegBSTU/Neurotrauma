7dc8e35b3401a813cfa0351eca4a7a6431acb211
themanyfaceddemon <neko-d-05@mail.ru>
2024-12-06 21:57:53 +0300

    i'll do it later

--- Neurotrauma/Lua/Scripts/types.lua
+++ Neurotrauma/Lua/Scripts/types.lua
@@ -12,3 +12,16 @@ NTTypes.BloodType = {
     ab_minus = "abminus",
     ab_plus = "abplus",
 }
+
+---@enum LimbTypes
+--- All types of limbs
+--- [1] - Type
+--- [2] - String representation
+NTTypes.LimbTypes = {
+    { LimbType.Torso,    "Torso" },
+    { LimbType.Head,     "Head" },
+    { LimbType.LeftArm,  "Left Arm" },
+    { LimbType.RightArm, "Right Arm" },
+    { LimbType.LeftLeg,  "Left Leg" },
+    { LimbType.RightLeg, "Right Leg" }
+}
