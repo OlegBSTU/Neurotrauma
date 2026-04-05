f85958f81f79963803baf13f4b9492faaa4172e5
ThousandFields <mattthejayfeatherfan@gmail.com>
2026-04-05 18:45:20 -0400

    make uncon check if player can always stay con

--- Neurotrauma/Lua/Scripts/Server/humanupdate.lua
+++ Neurotrauma/Lua/Scripts/Server/humanupdate.lua
@@ -953,13 +953,13 @@ NT.Afflictions = {
 					NTC.GetSymptom(c.character, i)
 					or c.stats.stasis
 					or c.afflictions.brainremoved.strength > 0
-					or (not HF.HasAffliction(c.character, "implacable", 0.05) and (c.character.Vitality <= 0 or c.afflictions.hypoxemia.strength > 80))
 					or c.afflictions.cerebralhypoxia.strength > 100
 					or c.afflictions.coma.strength > 15
 					or c.afflictions.t_arterialcut.strength > 0
 					or c.afflictions.seizure.strength > 0.1
 					or c.afflictions.opiateoverdose.strength > 60
 				)
+				and not c.character.HasAbilityFlag(AbilityFlags.AlwaysStayConscious)
 			c.afflictions[i].strength = HF.BoolToNum(isUnconscious, 2)
 		end,
 	},
