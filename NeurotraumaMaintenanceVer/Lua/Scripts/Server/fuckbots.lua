64d974fe24462a9245bb3a2aec45415849627901
OlegBSTU <polybox45@gmail.com>
2026-04-12 14:25:06 +0500

    init

---
+++ NeurotraumaMaintenanceVer/Lua/Scripts/Server/fuckbots.lua
@@ -0,0 +1,13 @@
+
+-- hopefully this stops bots from doing any rescuing at all.
+-- and also hopefully my assumption that this very specific thing
+-- about bots is what is causing them to eat frames is correct.
+
+if NTConfig.Get("NT_disableBotAlgorithms",true) then
+    Hook.Patch("Barotrauma.AIObjectiveRescueAll", "IsValidTarget", function (instance, ptable)
+        ptable.PreventExecution = true
+        return false
+    end, Hook.HookMethodType.Before)    
+end
+
+
