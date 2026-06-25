809860902b3b3eeaaadb6e802823c3ca2c23fde8
OlegBSTU <polybox45@gmail.com>
2026-06-25 14:32:47 +0500

    Remove testing file and add ru locals for ntsymbio

--- NT Symbiote/Lua/Autorun/init.lua
+++ NT Symbiote/Lua/Autorun/init.lua
@@ -1,27 +1,25 @@
-
 NTS = {} -- Neurotrauma Symbiote
-NTS.Name="Symbiote"
-NTS.Version = "A1.0.1"
+NTS.Name = "Symbiote"
+NTS.Version = "A1.0.2"
 NTS.VersionNum = 01000100
 NTS.MinNTVersion = "A1.12.1"
-NTS.MinNTVersionNum = 01120100
+NTS.MinNTVersionNum = 01120200
 NTS.Path = table.pack(...)[1]
-Timer.Wait(function() if NTC ~= nil and NTC.RegisterExpansion ~= nil then NTC.RegisterExpansion(NTS) end end,1)
+Timer.Wait(function()
+	if NTC ~= nil and NTC.RegisterExpansion ~= nil then NTC.RegisterExpansion(NTS) end
+end, 1)
 
 -- server-side code (also run in singleplayer)
 if (Game.IsMultiplayer and SERVER) or not Game.IsMultiplayer then
+	Timer.Wait(function()
+		if NTC == nil then
+			print("Error loading NT Symbiote: It appears Neurotrauma isn't loaded!")
+			return
+		end
 
-    Timer.Wait(function()
-        if NTC == nil then
-            print("Error loading NT Symbiote: It appears Neurotrauma isn't loaded!")
-            return
-        end
+		dofile(NTS.Path .. "/Lua/Scripts/helperfunctions.lua")
 
-        dofile(NTS.Path.."/Lua/Scripts/helperfunctions.lua")
-
-        dofile(NTS.Path.."/Lua/Scripts/humanupdate.lua")
-        dofile(NTS.Path.."/Lua/Scripts/items.lua")
-
-    end,1)
-
+		dofile(NTS.Path .. "/Lua/Scripts/humanupdate.lua")
+		dofile(NTS.Path .. "/Lua/Scripts/items.lua")
+	end, 1)
 end
