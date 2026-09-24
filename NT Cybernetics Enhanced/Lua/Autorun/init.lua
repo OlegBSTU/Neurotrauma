d525c414dc1c34a8325488be3e13339db643256d
OlegBSTU <polybox45@gmail.com>
2026-09-24 21:39:54 +0500

    Change version

--- NT Cybernetics Enhanced/Lua/Autorun/init.lua
+++ NT Cybernetics Enhanced/Lua/Autorun/init.lua
@@ -1,7 +1,7 @@
 NTCyb = {} -- Neurotrauma Cybernetics
 NTCyb.Name = "Cybernetics"
-NTCyb.Version = "A1.5.1"
-NTCyb.VersionNum = 01050100
+NTCyb.Version = "A1.5.2"
+NTCyb.VersionNum = 01050200
 NTCyb.MinNTVersion = "A1.9.0"
 NTCyb.MinNTVersionNum = 01090000
 NTCyb.Path = table.pack(...)[1]
